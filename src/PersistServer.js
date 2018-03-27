02186e0b19a2f9b342b19a014fae28f2c9d174c5
raymond <rsvvuuren@hotmail.com>
2018-03-27 12:01:39 +0200

    add json parse check

--- src/PersistServer.js
+++ src/PersistServer.js
@@ -8,11 +8,18 @@ export default ({store, storage, modules}) => {
   const promises = [];
   (typeof modules === 'string' ? [modules] : modules).map((module) => {
     promises.push(storage.getItem(module).then((item) => {
-      if (item !== null) {
-        store.dispatch({
-          type: `@@redux-persist-component/${module}`,
-          result: JSON.parse(item)
-        });
+
+      if (item !== null && item !== 'undefined') {
+        try {
+          const parsed = JSON.parse(item);
+          store.dispatch({
+            type: `@@redux-persist-component/${module}`,
+            result: parsed
+          });
+
+        } catch(error) {
+          console.error('Json parse failed: ', error);
+        }
       }
     }));
   });
